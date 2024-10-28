daa969bd284ff0216048c090d1cf6249a8cda41d
Martinelii <Kevenmartineli@outlook.com>
2024-10-28 11:39:13 -0300

    Exercico 1 - JAVA03/04

---
+++ src/exercicios_gen/Exercicio_1.java
@@ -0,0 +1,23 @@
+package exercicios_gen;
+import java.util.Scanner;
+
+public class Exercicio_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float salario, abono, novoSalario;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite o valor do salario: ");
+		salario = leia.nextFloat();
+		
+		System.out.println("Digite o valor do abono: ");
+		abono = leia.nextFloat();
+		
+		novoSalario = salario + abono;
+		
+		System.out.printf("Valor Atualizado do Salario R$%.2f", novoSalario);
+	}
+
+}
